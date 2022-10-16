1aa9d69e9de43029aee77684c7203ea41a37304e
Vajith Chamuditha <vajithc@gmail.com>
2022-10-16 14:40:22 +0530

    Initialize the spring project

---
+++ MyProjectOne/src/test/java/com/example/MyProjectOne/MyProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyProjectOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
